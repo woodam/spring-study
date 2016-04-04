db9989d93b37b07c4b76f72b0bd5c4bbd3060b0f
Ivan(지우담) <woodam@coupang.com>
2016-04-04 10:45:22 +0900

    no message

---
+++ ex00/src/test/java/org/zerock/web/DataSourceTest.java
@@ -0,0 +1,35 @@
+package org.zerock.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+	locations ={"file:ex00/src/main/webapp/WEB-INF/spring/**/*.xml"})
+public class DataSourceTest {
+
+	@Inject
+	private DataSource ds;
+
+
+
+	@Test
+	public void testConection()throws Exception{
+
+		try(Connection con = ds.getConnection()){
+
+			System.out.println(con);
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
+
+
